78ed396314465addad0f817c016cf059a15740de
katroll <kroll256@GMAIL.COM>
2022-04-17 15:02:36 -0400

    sorted tests in display list

--- src/components/TestList.js
+++ src/components/TestList.js
@@ -9,10 +9,7 @@ function TestList() {
     const { category } = useParams();
 
     const filteredQuizzes = quizzes.filter(quiz => quiz.category === category)
-
-    const sortedQuizzes = filteredQuizzes.sort();
-
-    console.log(sortedQuizzes);
+    const sortedQuizzes = filteredQuizzes.sort((a,b) => (a.name > b.name) ? 1 : -1);
 
     function capitalizeFirstLetter(string) {
         return string.charAt(0).toUpperCase() + string.slice(1);
@@ -24,7 +21,7 @@ function TestList() {
                     <div className="flex flex-col w-full pt-10 pl-12 items-center">
                         <h1 className="text-4xl text-th-title-text font-bold">{capitalizeFirstLetter(category)} Tests</h1>
 
-                        <QuizTable quizzes={filteredQuizzes} />
+                        <QuizTable quizzes={sortedQuizzes} />
 
                     </div>
             ) : (
