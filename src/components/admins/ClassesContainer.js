6ec489a130bb4429d633a605eb8381c0a3c5944e
katroll <kroll256@GMAIL.COM>
2022-04-19 13:49:13 -0400

    added classes page and add class form

---
+++ src/components/admins/ClassesContainer.js
@@ -0,0 +1,14 @@
+import CreateNewClass from "./CreateNewClass";
+
+
+function ClassesContainer() {
+
+    return (
+        <div className="flex flex-col pt-5">
+            <CreateNewClass />
+            <p>here are the classes</p>
+        </div>
+    )
+}
+
+export default ClassesContainer;
